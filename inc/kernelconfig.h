7577008c457fa7f4615f3b65381e52559b4efbc5
jamescarr0 <jamescarr0@hotmail.com>
2022-03-02 14:27:21 +0000

    Implement the IDT and a simple ISR / IRQ-0

---
+++ inc/kernelconfig.h
@@ -0,0 +1,14 @@
+/**
+ * @file kernelconfig.h
+ * @author James Carr
+ * @date 02/03/2022
+ * @brief Configuration settings for SimpleOS Kernel.
+ * 
+ */
+
+#ifndef SIMPLEOS_KERNELCONFIG_H
+#define SIMPLEOS_KERNELCONFIG_H
+
+#define SIMPLEOS_MAX_INTERRUPTS 512
+
+#endif
