bcef18a52f0c34fae87d59314b0de7b4fe0ee402
jamescarr0 <jamescarr0@hotmail.com>
2022-03-06 20:04:50 +0000

    Move documentation into memory.h complete

--- inc/memory.h
+++ inc/memory.h
@@ -1,16 +1,26 @@
 /**
- * @file kernel header for memory function prototypes.
- * @date 23/02/2022
- * **Author:** James Carr
- * ## Role.
- * Provides memory function prototypes for the memory operations.
+ * @file memory.h
+ * @author James Carr
+ * @brief Declerations for memory related tasks used by ther kernel
+ * @version 0.1
+ * @date 2022-02-23
+ * 
  */
+
 
 #ifndef SIMPLEOS_MEMORY_H
 #define SIMPLEOS_MEMORY_H
 
 #include <stddef.h>
 
-void *memset(void* ptr, int c, size_t size);
+/**
+ * Sets a block of memory of n bytes with the specified value.
+ *
+ * @param ptr Pointer to the starting address of the memory block to fill.
+ * @param c Value to fill the memory block with.
+ * @param size Number of bytes to set.
+ * @return void pointer to the starting address.
+ */
+void *memset(void *ptr, int c, size_t size);
 
-#endif //SIMPLEOS_MEMORY_H
+#endif // SIMPLEOS_MEMORY_H
--- src/memory/memory.c
+++ src/memory/memory.c
@@ -3,26 +3,20 @@
  * @author James Carr
  * @date 23/02/2022
  * @brief Source code for kernel memory function implementations.
- * 
- * The header file contains implementations for memory funcitons used by the kernel
- * 
+ *
+ * Implementations for memory related tasks used by the kernel
+ *
  */
 
 #include "memory.h"
 
-/**
- * Sets a block of memory of n bytes with the specified value.
- *
- * @param ptr Pointer to the starting address of the memory block to fill.
- * @param c Value to fill the memory block with.
- * @param size Number of bytes to set.
- * @return void pointer to the starting address.
- */
-void *memset(void* ptr, int c, size_t size) {
-    char *pC = (char *) ptr;
+/* Implementation of the C memset function */
+void *memset(void *ptr, int c, size_t size)
+{
+    char *pC = (char *)ptr;
 
     for (size_t i = 0; i < size; i++)
         pC[i] = c;
-    
+
     return ptr;
 }
